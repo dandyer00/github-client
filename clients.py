6ef7edc4b3f44f359ffd175b32fb03839ebd4c56
dandyer00 <ddyer@suse.com>
2017-08-09 16:39:35 -0600

    'refactoring'

--- clients.py
+++ clients.py
@@ -3,6 +3,10 @@ import json
 
 class githubClient:
     
+    def __init__(self, user):
+        self.user = user
+        self.gitHubURL = 'https://api.github.com/'
+        
     def requestAndParse(self, url):
         r = requests.get(url)
         if(r.ok):
@@ -10,16 +14,21 @@ class githubClient:
             return self.repoInfo
     
     def getOrgRepoList(self, org):
-        url = 'https://api.github.com/orgs/' + org + '/repos'
+        url = self.gitHubURL + 'orgs/' + org + '/repos'
         return self.requestAndParse(url)
              
-    def getUserRepoList(self, user):
-        url = 'https://api.github.com/users/' + user + '/repos'
+    def getUserRepoList(self):
+        url = self.gitHubURL + 'users/' + self.user + '/repos'
         return self.requestAndParse(url)
         
-    def getRepoInfo(self, url):
+    def getRepoInfo(self, repo):
+        url = self.gitHubURL + 'repos/' + self.user + '/' + repo
         return self.requestAndParse(url)
     
-    def getUserProfile(self, user):
-        url = 'https://api.github.com/users/' + user
+    def getUserProfile(self ):
+        url = self.gitHubURL + 'users/' + self.user
+        return self.requestAndParse(url)
+
+    def getRepoIssues(self, repo):
+        url = self.gitHubURL + 'repos/' + self.user + '/' + repo + '/issues/'
         return self.requestAndParse(url)
--- main.py
+++ main.py
@@ -2,29 +2,33 @@
 import clients
 
 if __name__ == '__main__':
-    g = clients.githubClient()
-    
-    url = 'https://api.github.com/repos/dandyer00/test1'
-    d= g.getRepoInfo(url)
-    print "RepoInfo for %s" % url
-    for k in d:
-        print '%s: %s' % (k, d[k])
-    
     user =  'dandyer00'   
-    print '\nUser Profile for %s' % user
-    d = g.getUserProfile(user)
-    for k in d:
-        print '%s: %s' % (k, d[k])    
-        
-    print '\nRepos for %s' % user
-    l = g.getUserRepoList(user)
-    for d in l:
-        for k in d:
-            print '%s: %s' % (k, d[k])    
-
+    repo = 'github-wiki-notification'
     org = 'SUSE'
-    print '\nRepos for %s' % org
-    l = g.getOrgRepoList(org)
-    for d in l:
-        for k in d:
-            print '%s: %s' % (k, d[k])        
+
+    g = clients.githubClient(user)
+      
+#     d= g.getRepoInfo(repo)
+#     print "RepoInfo for %s/%s" % (user, repo)
+#     for k in d:
+#         print '%s: %s' % (k, d[k])
+#     
+#     print '\nUser Profile for %s' % user
+#     d = g.getUserProfile()
+#     for k in d:
+#         print '%s: %s' % (k, d[k])    
+#         
+#     print '\nRepos for %s' % user
+#     l = g.getUserRepoList()
+#     for d in l:
+#         for k in d:
+#             print '%s: %s' % (k, d[k])    
+# 
+#     print '\nRepos for %s' % org
+#     l = g.getOrgRepoList(org)
+#     for d in l:
+#         for k in d:
+#             print '%s: %s' % (k, d[k])        
+            
+    print '\n Issues for %s/%s' % (user, repo)
+    l = g.getRepoIssues(repo)
